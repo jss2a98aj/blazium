350ee6996d8c5aeae8b186d4dc703363244b973a
jss2a98aj <jss2a98aj@gmail.com>
2024-12-13 01:37:59 -0500

    Misc Rebranding
    Co-Authored-By: Bioblaze Payne <BioblazePayne@gmail.com>
    Co-Authored-By: John Knight <80524176+tekisasu-johnk@users.noreply.github.com>
    Co-Authored-By: Christoffer Sundbom <13393087+norrox@users.noreply.github.com>
    Co-Authored-By: Nicholas Santos Shiden <95104877+sshiiden@users.noreply.github.com>

--- misc/scripts/install_d3d12_sdk_windows.py
+++ misc/scripts/install_d3d12_sdk_windows.py
@@ -117,4 +117,4 @@ print(f"DirectX 12 Agility SDK {agility_sdk_version} installed successfully.\n")
 
 # Complete message
 print(f'{Ansi.GREEN}All Direct3D 12 SDK components were installed to "{deps_folder}" successfully!{Ansi.RESET}')
-print(f'{Ansi.GREEN}You can now build Godot with Direct3D 12 support enabled by running "scons d3d12=yes".{Ansi.RESET}')
+print(f'{Ansi.GREEN}You can now build Blazium with Direct3D 12 support enabled by running "scons d3d12=yes".{Ansi.RESET}')
--- platform/android/java/editor/src/main/java/org/godotengine/editor/BaseGodotEditor.kt
+++ platform/android/java/editor/src/main/java/org/godotengine/editor/BaseGodotEditor.kt
@@ -355,7 +355,7 @@ abstract class BaseGodotEditor : GodotActivity() {
 		for (runningProcess in runningProcesses) {
 			if (runningProcess.processName == processName) {
 				// Killing process directly
-				Log.v(TAG, "Killing Godot process ${runningProcess.processName}")
+				Log.v(TAG, "Killing Blazium process ${runningProcess.processName}")
 				Process.killProcess(runningProcess.pid)
 				return true
 			}
