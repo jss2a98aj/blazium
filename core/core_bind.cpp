8b807b237161da8f1f04560a17965c16b41db6cd
Mister Puma <MisterPuma80@gmail.com>
2025-02-02 20:49:39 -0800

    Exposed Engine.get_frame_ticks to GDScript.

--- core/core_bind.cpp
+++ core/core_bind.cpp
@@ -1936,6 +1936,10 @@ double Engine::get_time_scale() {
 	return ::Engine::get_singleton()->get_time_scale();
 }
 
+uint64_t Engine::get_frame_ticks() const {
+	return ::Engine::get_singleton()->get_frame_ticks();
+}
+
 int Engine::get_frames_drawn() {
 	return ::Engine::get_singleton()->get_frames_drawn();
 }
@@ -2096,6 +2100,7 @@ void Engine::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("set_time_scale", "time_scale"), &Engine::set_time_scale);
 	ClassDB::bind_method(D_METHOD("get_time_scale"), &Engine::get_time_scale);
 
+	ClassDB::bind_method(D_METHOD("get_frame_ticks"), &Engine::get_frame_ticks);
 	ClassDB::bind_method(D_METHOD("get_frames_drawn"), &Engine::get_frames_drawn);
 	ClassDB::bind_method(D_METHOD("get_frames_per_second"), &Engine::get_frames_per_second);
 	ClassDB::bind_method(D_METHOD("get_physics_frames"), &Engine::get_physics_frames);
