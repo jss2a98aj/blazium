8d615be22f2069ed8e3f9d69df9b1cbcf0f62fe5
jss2a98aj <jss2a98aj@gmail.com>
2025-04-24 05:27:24 -0400

    Add GodotSteam module
    Co-Authored-By: Dragos Daian <daian.dragos@yahoo.com>
    Co-Authored-By: Mounir Tohami <53877170+WhalesState@users.noreply.github.com>

---
+++ modules/godotsteam/register_types.cpp
@@ -0,0 +1,21 @@
+#include "register_types.h"
+#include "core/config/engine.h"
+#include "core/object/class_db.h"
+#include "godotsteam.h"
+
+static Steam *SteamPtr = NULL;
+
+void initialize_godotsteam_module(ModuleInitializationLevel level) {
+	if (level == MODULE_INITIALIZATION_LEVEL_SERVERS) {
+		GDREGISTER_CLASS(Steam);
+		SteamPtr = memnew(Steam);
+		Engine::get_singleton()->add_singleton(Engine::Singleton("Steam", Steam::get_singleton()));
+	}
+}
+
+void uninitialize_godotsteam_module(ModuleInitializationLevel level) {
+	if (level == MODULE_INITIALIZATION_LEVEL_SERVERS) {
+		Engine::get_singleton()->remove_singleton("Steam");
+		memdelete(SteamPtr);
+	}
+}
---
+++ modules/godotsteam/register_types.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "modules/register_module_types.h"
+
+void initialize_godotsteam_module(ModuleInitializationLevel level);
+void uninitialize_godotsteam_module(ModuleInitializationLevel level);
