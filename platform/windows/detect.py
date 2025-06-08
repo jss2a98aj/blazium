6474a8b61a8e6421daa6107d6b9bbf679209e990
jss2a98aj <jss2a98aj@gmail.com>
2025-06-08 04:24:25 -0400

    Restore support for Windows 7/8/8.1

--- platform/windows/detect.py
+++ platform/windows/detect.py
@@ -5,7 +5,7 @@ import sys
 from typing import TYPE_CHECKING
 
 import methods
-from methods import print_error, print_warning
+from methods import print_error, print_info, print_warning
 from platform_methods import detect_arch, validate_arch
 
 if TYPE_CHECKING:
@@ -182,6 +182,14 @@ def get_opts():
 
     return [
         ("mingw_prefix", "MinGW prefix", mingw),
+        # Targeted Windows version: 7 (and later), minimum supported version
+        # XP support dropped after EOL due to missing API for IPv6 and other issues
+        # Vista support dropped after EOL due to GH-10243
+        (
+            "target_win_version",
+            "Targeted Windows version, >= 0x0601 (Windows 7)",
+            "0x0601",
+        ),
         EnumVariable("windows_subsystem", "Windows subsystem", "gui", ["gui", "console"], ignorecase=2),
         ("msvc_version", "MSVC version to use. Handled automatically by SCons if omitted.", ""),
         BoolVariable("use_mingw", "Use the Mingw compiler, even if MSVC is installed.", False),
@@ -353,6 +361,13 @@ def configure_msvc(env: "SConsEnvironment"):
     env.AppendUnique(CCFLAGS=["/utf-8"])  # Force to use Unicode encoding.
     env.AppendUnique(CCFLAGS=["/bigobj"])  # Support big objects.
 
+    validate_win_version(env)
+
+    if env["accesskit"]:
+        if int(env["target_win_version"], 16) < 0x0602:
+            print_info("AccessKit enabled, targeted Windows version changed to Windows 8 (0x602).")
+            env["target_win_version"] = "0x0602"  # Accessibility API require Windows 8+
+
     env.AppendUnique(
         CPPDEFINES=[
             "WINDOWS_ENABLED",
@@ -360,8 +375,8 @@ def configure_msvc(env: "SConsEnvironment"):
             "WINMIDI_ENABLED",
             "TYPED_METHOD_BIND",
             "WIN32",
-            ("WINVER", "0x0A00"),
-            ("_WIN32_WINNT", "0x0A00"),
+            ("WINVER", env["target_win_version"]),
+            ("_WIN32_WINNT", env["target_win_version"]),
         ]
     )
     env.AppendUnique(CPPDEFINES=["NOMINMAX"])  # disable bogus min/max WinDef.h macros
@@ -390,7 +405,6 @@ def configure_msvc(env: "SConsEnvironment"):
         "gdi32",
         "IPHLPAPI",
         "Shlwapi",
-        "Shcore",
         "wsock32",
         "Ws2_32",
         "shell32",
@@ -716,6 +730,13 @@ def configure_mingw(env: "SConsEnvironment"):
 
     ## Compile flags
 
+    validate_win_version(env)
+
+    if env["accesskit"]:
+        if int(env["target_win_version"], 16) < 0x0602:
+            print_info("AccessKit enabled, targeted Windows version changed to Windows 8 (0x602).")
+            env["target_win_version"] = "0x0602"  # Accessibility API require Windows 8+
+
     if not env["use_llvm"]:
         env.Append(CCFLAGS=["-mwindows"])
 
@@ -746,8 +767,8 @@ def configure_mingw(env: "SConsEnvironment"):
     env.Append(CPPDEFINES=["WINDOWS_ENABLED", "WASAPI_ENABLED", "WINMIDI_ENABLED"])
     env.Append(
         CPPDEFINES=[
-            ("WINVER", "0x0A00"),
-            ("_WIN32_WINNT", "0x0A00"),
+            ("WINVER", env["target_win_version"]),
+            ("_WIN32_WINNT", env["target_win_version"]),
         ]
     )
     env.Append(
@@ -761,7 +782,6 @@ def configure_mingw(env: "SConsEnvironment"):
             "iphlpapi",
             "shell32",
             "shlwapi",
-            "shcore",
             "wsock32",
             "ws2_32",
             "kernel32",
@@ -928,4 +948,10 @@ def check_d3d12_installed(env, suffix):
             "\thttps://docs.godotengine.org/en/latest/engine_details/development/compiling/compiling_for_windows.html\n"
             "Alternatively, disable this driver by compiling with `d3d12=no` explicitly."
         )
+        sys.exit(255)
+
+
+def validate_win_version(env):
+    if int(env["target_win_version"], 16) < 0x0601:
+        print_error("`target_win_version` should be 0x0601 or higher (Windows 7).")
         sys.exit(255)
